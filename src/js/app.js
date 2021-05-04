29086931fd15e4fa5d85fc3feb2cf892bcf29d99
jomas94 <jomariojsa@gmail.com>
2021-05-04 11:49:10 +0200

    dark mode

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    eventListener();
+    darkMode();
+});
+
+function darkMode() {
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+    });
+
+}
+
+function eventListener() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click',navegacionResponsive);
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector('.navegacion');
+
+    navegacion.classList.toggle('mostrar');
+
+}
